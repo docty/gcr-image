164af28e7b35395905e41348eedde18e0c19ae2a
Henry <sensork82@gmail.com>
2025-04-08 14:12:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,29 +4,7 @@ import numpy as np
 import torch.nn as nn
 import streamlit as st
 
-# Generator model
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
-        self.main = nn.Sequential(
-            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
-            nn.Tanh()
-        )
 
-    def forward(self, x):
-        return self.main(x)
 
 # Load model and define the device
 generator_checkpoint_path = "generator.pth"  # Replace with the correct checkpoint file path
