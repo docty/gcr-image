10ed99f559c457319485c30d0ab003b720b2e4ab
Henry <sensork82@gmail.com>
2025-04-08 10:08:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
-import streamlit as st
+import streamlit as st 
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch import nn
+from torchvision.utils import save_image
+from io import BytesIO
 
-st.title("🎈 Good morning everyone")
+
+st.title("DCGAN")
 st.write(
     "We are about to deploy a quick pytorch project"
 )
