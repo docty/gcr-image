a7eba580baf41fa2ac89633f22fc3722fc7531f3
Henry <sensork82@gmail.com>
2025-04-08 10:08:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Good morning everyone")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "We are about to deploy a quick pytorch project"
 )
